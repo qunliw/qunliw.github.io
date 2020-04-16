4fef2188d018a737cb35f82a6a66e1a2062f04fc
qunliw <62570840+qunliw@users.noreply.github.com>
2020-04-16 17:49:20 -0500

    Updates

--- precache-manifest.48850144fdf930bcf77396d0a7ab313c.js
+++ precache-manifest.48850144fdf930bcf77396d0a7ab313c.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "91879fa1d96b40d4d4f384a2677b2108",
+    "revision": "239c5b9dcf1a1e6bae9c74a26108bc34",
     "url": "/index.html"
   },
   {
-    "revision": "1a8fb3d03d2d10627f06",
-    "url": "/static/js/2.59183114.chunk.js"
+    "revision": "f87b01b885531318e42b",
+    "url": "/static/js/2.3941ef4a.chunk.js"
   },
   {
     "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
-    "url": "/static/js/2.59183114.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.3941ef4a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "872cf9dfd59efb1e423f",
-    "url": "/static/js/main.63cba002.chunk.js"
+    "revision": "efad52ffe11ab01c7d1e",
+    "url": "/static/js/main.4d010507.chunk.js"
   },
   {
     "revision": "a37778fe0935a06a7a1f",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4ae7ff4a1603b360f8f88e948afca82c.js"
+  "/precache-manifest.48850144fdf930bcf77396d0a7ab313c.js"
 );
 
 self.addEventListener('message', (event) => {
